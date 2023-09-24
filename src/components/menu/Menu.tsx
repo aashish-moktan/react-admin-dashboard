b9ced329d26d1b0de7dfa4942ba4dd8e6b8b8420
twobits <twobits@pop-os.localdomain>
2023-09-24 17:50:19 +0545

    Setup menu design

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import { menus } from "../../data/menus";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      {menus.map((menu) => (
+        <div className="item" key={menu.id}>
+          <span className="menuTitle">{menu.title}</span>
+          {menu.listItems.map((item) => (
+            <Link to={item.url} key={item.id} className="listItem">
+              <img src={item.icon} alt="" />
+              <span className="listItemTitle">{item.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
