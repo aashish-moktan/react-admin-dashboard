076537debfdcbf275e0dbedda96c068f60325469
twobits <twobits@pop-os.localdomain>
2023-09-24 16:48:30 +0545

    Setup layout and add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">Content</div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+  ]);
 
-  return (
-    <div>Hello World</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
